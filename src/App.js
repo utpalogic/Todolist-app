1308d60bce873262c0da9c0a1059204e31d1e987
Utpala khatri <141339568+utpalogic@users.noreply.github.com>
2024-08-08 22:09:23 +0545

    A feature to delete unwanted todo list has been added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import TodoForm from './Components/TodoForm';
 import TodoList from './Components/TodoList';
-import './App.css'; // Global styles
+import './App.css';
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -10,12 +10,16 @@ function App() {
     setTodos([...todos, todo]);
   };
 
+  const deleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
   return (
     <div className="app-container">
       <div className="square-container">
         <TodoForm addTodo={addTodo} />
         <div className="todo-list-wrapper">
-          <TodoList todos={todos} />
+          <TodoList todos={todos} onDelete={deleteTodo} />
         </div>
       </div>
     </div>
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import './TodoList.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDelete }) => {
   return (
-    <div className="todo-list-container">
-      <div className="todo-list">
-        {todos.map((todo, index) => (
-          <div key={index} className="todo">
-            <h3 title={todo.title}>{todo.title}</h3>
-            <p className="description" title={todo.description}>{todo.description}</p>
-            <div className="tooltip">
-              <strong>{todo.title}</strong><br/><br/>{todo.description}
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="todo-list">
+      {todos.map((todo, index) => (
+        <div key={index} className="todo">
+          <button className="delete-button" onClick={() => onDelete(index)}>X</button>
+          <h3 title={todo.title}>{todo.title}</h3>
+          <p className="description" title={todo.description}>{todo.description}</p>
+        </div>
+      ))}
     </div>
   );
 };
